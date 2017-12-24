321e5f616307b6c816f356a7773cb014f179f64c
vimalpatel87 <vimalpatel@vimals-MacBook-Pro.local>
2017-12-24 00:51:20 -0600

    Quiz Results Display and Share
    Quiz Results Display and Share

---
+++ js/quiz.js
@@ -0,0 +1,75 @@
+(function(){
+    var app = angular.module('myQuiz',[]);
+    app.controller('quizController',['$scope','$http','$sce',function($scope,$http,$sce){
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
+        
+        $http.get('quiz_data.json').then(function(quizData){
+            $scope.myQuestions = quizData.data;   
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
+        $scope.changeActiveQuestion = function(){
+            $scope.activeQuestion = 0;
+        }
+        $scope.selectAnswer = function(qIndex,aIndex){
+            
+            var questionState = $scope.myQuestions[qIndex].questionState;
+            
+            if(questionState != 'answered'){
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                var correctAnswer = $scope.myQuestions[qIndex].correct;
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+                if(aIndex === correctAnswer){
+                    $scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score += 1;
+                } else{
+                    $scope.myQuestions[qIndex].correctness = 'incorrect';
+                }
+                $scope.myQuestions[qIndex].questionState = 'answered';
+            }
+            $scope.percentage = (($scope.score * 100)/$scope.totalQuestions).toFixed(2);
+        }
+        
+        
+        $scope.isSelected = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        }
+        
+        $scope.isCorrect = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        }
+        
+        $scope.selectContinue = function(){
+            
+            $scope.activeQuestion += 1;
+        }
+        
+        $scope.createShareLinks = function(percentage){
+            var url = 'http://codifydesign.com';
+            var emailLink = '<a class="btn email" href="mailto:metataggsolutions@gmail.com?subject=Try to beat my quiz score!&amp;body=I scored a ' + percentage + ' on this quiz about Saturn. try to beat my score at ' + url + '">Email a Friend</a>';
+            var twitterLink = '<a class="btn twitter" target="_blank" href="http://twitter.com/share?text=I scored '+percentage+' on this quiz about Saturn.try to beat my score at&amp;hashtags=SaturnQuiz&url='+url+'">Tweet your score</a>';
+            var newMarkup = emailLink + twitterLink;
+            return $sce.trustAsHtml(newMarkup);
+        }
+    }]);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
